58280f47953bc4c6658c53bd8e831a7d9f807694
dxlliv <hello@dxlliv.dev>
2024-12-24 00:56:08 +0100

    Implement players count animation in ServerDetailPlayers.vue

--- src/components/Server/Detail/ServerDetailPlayers.vue
+++ src/components/Server/Detail/ServerDetailPlayers.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  server: IServer
+}>()
+</script>
+
 <template>
   <div class="minehub-server-detail__players">
     <div v-if="server.online">
-      <span class="players" v-text="server.players"/>
+      <span class="players" />
       <span class="slots text-grey" v-text="' / ' + server.slots"/>
     </div>
 
@@ -12,17 +18,27 @@
   </div>
 </template>
 
-<script setup lang="ts">
-defineProps<{
-  server: IServer
-}>()
-</script>
-
 <style scoped lang="scss">
+@property --num {
+  syntax: '<integer>';
+  initial-value: 0;
+  inherits: false;
+}
+
 .minehub-server-detail__players {
   @media(max-width: 959px) {
     position: absolute;
     right: 0;
+  }
+
+  .players {
+    transition: --num 1s;
+    counter-reset: num var(--num);
+    --num: v-bind("server.players");
+
+    &::after {
+      content: counter(num);
+    }
   }
 }
 </style>
