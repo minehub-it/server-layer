b71fb24e728db98aeb0092763c6acc5012332f98
dxlliv <hello@dxlliv.dev>
2024-12-23 15:43:02 +0100

    Fix slider vertical spacing

--- src/pages/featured/index.vue
+++ src/pages/featured/index.vue
@@ -7,7 +7,7 @@ definePageMeta({
 <template>
   <SliderVertical
       :slides-per-view="2.5" :space-between="24"
-      class="px-6"
+      class="px-6 py-md-6"
   >
     <swiper-slide>
       <ServerFeaturedLargeAdd />
