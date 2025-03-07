4c8ab8c528bf81a9d2530d7d04c7ee39195b6829
Nicolas Giuliani <nicolas.giuliani@hotmail.com>
2025-03-07 15:07:33 +0100

    try fallback with mcsrvstat

--- src/server/api/ping/[address]/[platform].ts
+++ src/server/api/ping/[address]/[platform].ts
@@ -19,18 +19,44 @@ async function pingWithRetry(
   throw lastError;
 }
 
+async function pingMcsrvstat(platform: string, address: string): Promise<any> {
+  let url = '';
+  if (platform === 'je') {
+    url = `https://api.mcsrvstat.us/2/${address}`;
+  } else if (platform === 'be') {
+    url = `https://api.mcsrvstat.us/bedrock/${address}`;
+  } else {
+    throw new Error('Invalid platform');
+  }
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Failed to fetch from mcsrvstat.us');
+  }
+  const data = await response.json();
+  return data;
+}
+
 async function tryPing(platform: string, address: string): Promise<any> {
   if (platform === 'je') {
     try {
       return await pingWithRetry(pingJava, address, 2);
-    } catch (err) {
-      return await pingBedrock(address);
+    } catch (err1) {
+      try {
+        return await pingWithRetry(pingBedrock, address, 2);
+      } catch (err2) {
+        return await pingMcsrvstat(platform, address);
+      }
     }
   } else if (platform === 'be') {
     try {
       return await pingWithRetry(pingBedrock, address, 2);
-    } catch (err) {
-      return await pingJava(address);
+    } catch (err1) {
+      try {
+        return await pingWithRetry(pingJava, address, 2);
+      } catch (err2) {
+        return await pingMcsrvstat(platform, address);
+      }
     }
   }
   throw new Error('Invalid platform');
@@ -67,10 +93,15 @@ export default defineEventHandler(async (event) => {
 
   try {
     const serverPing = await tryPing(platform, address);
-    result.online = true;
-    result.players = serverPing.players.online;
-    result.slots = serverPing.players.max;
-    result.favicon = serverPing.favicon;
+    if (serverPing.online) {
+      result.online = true;
+      result.players = serverPing.players?.online ?? 0;
+      result.slots = serverPing.players?.max ?? 0;
+      result.favicon = serverPing.favicon ?? (serverPing.icon ?? '');
+    } else {
+      result.online = false;
+      result.players = 0;
+    }
   } catch (err) {
     result.online = false;
     result.players = 0;
