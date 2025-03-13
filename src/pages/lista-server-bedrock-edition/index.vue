fb1d5e5b97c62e69dcc1d9880910effbc33e2014
dxlliv <hello@dxlliv.dev>
2025-03-13 20:57:00 +0100

    Aah there was a reason things were there

--- src/pages/lista-server-bedrock-edition/index.vue
+++ src/pages/lista-server-bedrock-edition/index.vue
@@ -3,20 +3,11 @@
 </template>
 
 <script setup lang="ts">
-const serverListStore = useServerListStore()
-const serverCategoryStore = useServerCategoryStore()
 const serverFilterStore = useServerFilterStore()
 
-await serverCategoryStore.initialize()
-await serverListStore.fetchFromContent()
-
-serverFilterStore.setPlatform('je')
+serverFilterStore.setPlatform('be')
 serverFilterStore.setCategory(null)
 serverFilterStore.setFavorite(false)
-
-onMounted(async () => {
-  await serverListStore.fetchPlayers()
-})
 
 useHead({
   title: 'Lista Server per Minecraft: Bedrock Edition - Minecraft ITALIA',
